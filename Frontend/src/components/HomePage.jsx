075bdcd11fa7017a3955c9764b239dc2af472eff
vishalBelwal <vishalbelwal52@gmail.com>
2024-06-15 23:56:03 +0530

    completed

--- Frontend/src/components/HomePage.jsx
+++ Frontend/src/components/HomePage.jsx
@@ -110,12 +110,12 @@ function Homepage() {
         <div className="navbar w-full h-1/10 bg-black bg-opacity-30 flex items-center justify-end">
           <div className="w-1/2 flex h-full">
 
-            <div className="w-3/5 flex justify-around items-center">
+            {/* <div className="w-3/5 flex justify-around items-center">
               <ButtonHover text={"Premium"}/>
               <ButtonHover text={"Support"}/>
               <ButtonHover text={"Download"}/>
               <div className="h-1/2 border-r border-white"></div>
-            </div>
+            </div> */}
             
             <div className="w-2/5 flex justify-around h-full items-center">
               <a href="/signup"><ButtonHover text={"Sign up"}/></a>
