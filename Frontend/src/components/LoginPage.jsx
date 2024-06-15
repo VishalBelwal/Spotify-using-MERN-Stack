aa60de7a2f037a59922cf68b7cde2bfa28b5f2f0
vishalBelwal <vishalbelwal52@gmail.com>
2024-06-16 00:22:50 +0530

    class changes and some other changes

--- Frontend/src/components/LoginPage.jsx
+++ Frontend/src/components/LoginPage.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { useState } from "react";
 import { Icon } from "@iconify/react";
+// import spotify_white from "../components/assets/logo/spotify_white.svg";
+import logo from '../../public/logo.png';
 import Input from "./Shared/Input";
 import PassInput from "./Shared/PassInput";
 import { useGoogleLogin } from "@react-oauth/google";
@@ -19,7 +21,7 @@ function LoginPage() {
 
   const login = async () => {
     
-    const data = {email, password};
+    const data = {firstName, lastName, email, password};
     
     const response = await makeUnauthenticatedPOSTRequest("/auth/login", data);
     if(response && !response.err){
@@ -54,61 +56,95 @@ function LoginPage() {
   });
 
   return (
-    <div className=" h-full flex flex-col items-center">
-      <div className="logo p-3 border-b border-solid border-gray-300 w-full flex justify-center">
-        <Icon icon="logos:spotify" width={170} />
-      </div>
+    <div className="bg-app-black text-white flex items-center justify-center">
+      <div className="w-full h-full flex flex-col items-center">
+        <div className="login box_shadow m-8 shadow bg-black rounded-3xl">
+          <div className="logo p-3 border-gray-300 w-full flex justify-center">
+            <img src={logo} style={{ width: 50 }} />
+          </div>
 
-      <div className="inputField w-1/3 py-1 ">
-        <div className="font-bold mt-3 flex flex-col items-center text-lg">
-          To Continue, Login to Spotify
+          <div className="inputRegion px-8 m-5 flex items-center justify-center flex-col">
+            <div className="inputField font-bold m-2 text-lg">
+              <div className="font-bold mt-3 flex flex-col items-center text-lg">
+                To Continue, Login to Spotify
+              </div>
+
+              <div>
+                <button
+                  className="bg-black border text-sm border-gray-400 text-white font-bold w-full flex items-center justify-center p-2 rounded-full my-5"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    logIn();
+                  }}
+                >
+                  <Icon icon="ic:baseline-apple" width="40" height="30" />
+                  CONTINUE WITH APPLE
+                </button>
+              </div>
+
+              <div>
+                <button
+                  className="border border-gray-400 text-sm text-gray-600 font-bold w-full flex items-center justify-center p-2 rounded-full my-5"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    logIn();
+                  }}
+                >
+                  <Icon icon="flat-color-icons:google" width="40" height="30" />
+                  SIGN IN WITH GOOGLE
+                </button>
+              </div>
+
+              <div className="flex justify-center items-center">
+                <span className="w-full h-0 border border-solid mb-4 border-gray-500" />
+              </div>
+
+              <Input
+                label="Email address or userame"
+                placeholder="Email address or username"
+                className="my-2"
+                value={email}
+                setValue={setEmail}
+              />
+              <PassInput
+                label="Password"
+                placeholder="Password"
+                value={password}
+                setValue={setPassword}
+              />
+
+              <div className="flex items-center justify-center my-6  ">
+                <button
+                  className="bg-green-500 rounded-full font-semibold p-3 px-7 hover:bg-green-600"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    login();
+                  }}
+                >
+                  LOG IN
+                </button>
+              </div>
+
+              <div className="flex justify-center items-center">
+                <span className="w-20 h-0 border border-solid border-gray-500" />
+                  <div className="m-3 text-gray-300">or</div>
+                <span className="w-20 h-0 border border-solid border-gray-500" />
+              </div>
+
+              <div className="m-2 font-bold mb-4 flex flex-col items-center text-lg">
+                Don't have an account?
+              </div>
+
+              <div className="text-gray-500 w-full flex items-center justify-center rounded-full font-bold">
+                <a href="/signup">SIGNUP FOR SPOTIFY</a>
+              </div>
+
+              {/* <div className="border border-gray-400 text-gray-600 font-bold w-full flex items-center justify-center p-2 rounded-full">
+                
+              </div> */}
+            </div>
+          </div>
         </div>
-
-        <div>
-        <button className="bg-black text-white font-bold w-full flex items-center justify-center p-2 rounded-full my-5" onClick={(e) => {e.preventDefault(); logIn()}}>
-        <Icon icon="ic:baseline-apple" width="45" height="30"  />CONTINUE WITH APPLE
-        </button>
-      </div>
-
-        <div>
-        <button className="border border-gray-400 text-gray-600 font-bold w-full flex items-center justify-center p-2 rounded-full my-5" onClick={(e) => {e.preventDefault(); logIn()}}>
-        <Icon icon="flat-color-icons:google" width="45" height="30" />SIGN IN WITH GOOGLE
-        </button>
-      </div>
-
-      <div>
-        <button className="border border-gray-400 text-gray-600 font-bold w-full flex items-center justify-center p-2 rounded-full" onClick={(e) => {e.preventDefault(); logIn()}}>
-          CONTINUE WITH PHONE NUMBER
-        </button>
-      </div>
-
-        <Input
-          label="Email address or userame"
-          placeholder="Email address or username"
-          className="my-2"
-          value={email}
-          setValue={setEmail}
-        />
-        <PassInput label="Password" placeholder="Password" value={password} setValue={setPassword}/>
-
-        <div className="flex items-center justify-end my-6 ">
-          <button className="bg-green-500 rounded-full font-semibold p-3 px-7"
-            onClick={(e) => {
-              e.preventDefault();
-              login();
-            }}>LOG IN</button>
-        </div>
-
-        <div className="w-full border border-solid border-gray-300"></div>
-
-        <div className="m-2 font-bold mb-9 flex flex-col items-center text-lg">
-          Don"t have an account?
-        </div>
-
-        <div className="border border-gray-400 text-gray-600 font-bold w-full flex items-center justify-center p-2 rounded-full">
-          <a href="/signup">SIGNUP FOR SPOTIFY</a>
-        </div>
-
       </div>
     </div>
   );
--- Frontend/src/components/SignupPage.jsx
+++ Frontend/src/components/SignupPage.jsx
@@ -2,10 +2,13 @@ import React from "react";
 import { useState } from "react";
 import {useCookies} from "react-cookie";
 import { Icon } from "@iconify/react";
+// import spotify_white from "../components/assets/logo/spotify_white.svg";
+import logo from '../../public/logo.png';
 import Input from "./Shared/Input";
 import PassInput from "./Shared/PassInput";
 import { useNavigate } from "react-router-dom";
 import {makeUnauthenticatedPOSTRequest} from "../utils/ServerHelper";
+import './box_signin.scss';
 
 function SignupPage() {
 
@@ -39,25 +42,27 @@ function SignupPage() {
 }
 
   return (
-    <div className="w-full h-full flex flex-col items-center">
-        <div className="logo p-5 border-b border-solid border-gray-300 w-full flex justify-center">
-            <Icon icon="logos:spotify" width={170} />
+    <div className="bg-app-black text-white flex items-center justify-center">
+        <div className="w-full h-full flex flex-col items-center">
+        <div className="darker box_shadow m-8 shadow bg-black rounded-3xl" style={{}}>
+        <div className="logo p-4 w-full flex justify-center">
+            <img src={logo} style={{width: 50}} />
         </div>
-        <div className="inputRegion w-1/3 py-10 flex items-center justify-center flex-col">
+        <div className="inputRegion px-8 m-5 flex justify-center items-center flex-col">
             {/*  I will have my 2 inputs(email and password) and I will have my sign up instead button*/}
-            <div className="font-bold mb-4 text-2xl">
+            <div className="font-bold m-2 text-lg">
                 Sign up for free to start listening.
             </div>
             <Input
-                label="Email address"
-                placeholder="Enter your email"
-                className="my-6"
+                label="Email Address"
+                placeholder="name@domain.com"
+                className="my-3"
                 value={email}
                 setValue={setEmail}
             />
             <Input
                 label="Confirm Email Address"
-                placeholder="Enter your email again"
+                placeholder="name@domain.com"
                 className="mb-6"
                 value={confirmEmail}
                 setValue={setConfirmEmail}
@@ -65,35 +70,36 @@ function SignupPage() {
             <Input
                 label="Username"
                 placeholder="Enter your username"
-                className="mb-6"
+                className="mb-3"
                 value={userName}
                 setValue={setUsername}
             />
             <PassInput
                 label="Create Password"
-                placeholder="Enter a strong password here"
+                placeholder="Enter a strong password"
                 value={password}
                 setValue={setPassword}
             />
+            
             <div className="w-full flex justify-between items-center space-x-8">
                 <Input
                     label="First Name"
                     placeholder="Enter Your First Name"
-                    className="my-6"
+                    className="my-3"
                     value={firstName}
                     setValue={setFirstName}
                 />
                 <Input
                     label="Last Name"
                     placeholder="Enter Your Last Name"
-                    className="my-6"
+                    className="my-3"
                     value={lastName}
                     setValue={setLastName}
                 />
             </div>
-            <div className=" w-full flex items-center justify-center     my-8">
+            <div className=" w-full flex items-center justify-center my-4">
                 <button
-                    className="bg-green-400 font-semibold p-3 px-10 rounded-full"
+                    className="bg-green-500 font-semibold p-3 px-10 rounded-full hover:bg-green-600"
                     onClick={(e) => {
                         e.preventDefault();
                         signUp();
@@ -102,14 +108,23 @@ function SignupPage() {
                     Sign Up
                 </button>
             </div>
-            <div className="w-full border border-solid border-gray-300"></div>
-            <div className="my-6 font-semibold text-lg">
+
+            <div className="flex justify-center items-center">
+                <span className="w-28 h-0 border border-solid border-gray-500" />
+                  <div className="m-3 text-gray-500">or</div>
+                <span className="w-28 h-0 border border-solid border-gray-500" />
+            </div>
+
+            <div className="my-3 font-semibold text-lg items-center">
                 Already have an account?
             </div>
-            <div className="border border-gray-500 text-gray-500 w-full flex items-center justify-center py-4 rounded-full font-bold">
+            <div className="text-gray-500 w-full flex items-center justify-center py-1 rounded-full font-bold">
                 <a href="/login">LOG IN INSTEAD</a>
             </div>
         </div>
+        </div>
+    </div>
+        
     </div>
 );
 }
