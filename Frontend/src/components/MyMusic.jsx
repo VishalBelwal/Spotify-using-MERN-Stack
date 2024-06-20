a09c243f0a4aad39ab7f2fdba3130102e8ba42ae
vishalBelwal <vishalbelwal52@gmail.com>
2024-06-20 17:47:04 +0530

    changes

--- Frontend/src/components/MyMusic.jsx
+++ Frontend/src/components/MyMusic.jsx
@@ -1,17 +1,14 @@
 import React from "react";
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 import SingleSongCard from "./Shared/SingleSongCard";
 import { makeAuthenticatedGETRequest } from "../utils/ServerHelper";
 import LoggedInContainer from "./Container/Loggedincontainer";
 
-
-
-
 const MyMusic = () => {
-  const [songData, setSongData] = useState([]);    //ye api se fetch hokar aane wala data ke liye hai
+  const [songData, setSongData] = useState([]); //ye api se fetch hokar aane wala data ke liye hai
 
   useEffect(() => {
-    const getData = async() => {
+    const getData = async () => {
       const response = await makeAuthenticatedGETRequest("/song/get/mySongs");
       setSongData(response.data);
     };
@@ -20,17 +17,22 @@ const MyMusic = () => {
 
   return (
     <LoggedInContainer currentActivescreen="mymusic">
-      <div className="text-white text-xl font-semibold pb-4 pl-2 pt-8">My Songs</div>
-
-      <div className="space-y-3 overflow-auto">
-        {songData.map((item) => {
-          return <SingleSongCard info={item} playsound={() => {}} />;
-        })}
-      </div>
+      {songData ? (
+        <div>
+          <div className="text-white text-xl font-semibold pb-4 pl-2 pt-8">
+            My Songs
+          </div>
+          <div className="space-y-3 overflow-auto">
+            {songData.map((item) => {
+              return <SingleSongCard info={item} playsound={() => {}} />;
+            })}
+          </div>
+        </div>
+      ) : (
+        <div className="text-white text-center">Go to upload songs and Add more Songs</div>
+      )}
     </LoggedInContainer>
   );
-}
-
-
+};
 
 export default MyMusic;
