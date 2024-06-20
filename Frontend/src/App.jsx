2d2a6d43298917867ea53752109d2378b28e1bd4
vishalBelwal <vishalbelwal52@gmail.com>
2024-06-20 19:36:05 +0530

    finalised

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,7 +3,7 @@ import LoginPage from "./components/LoginPage";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { useState } from "react";
 import SignupPage from "./components/SignupPage";
-import HomePage from "./components/HomePage";
+import Main from "./components/Main";
 import { useCookies } from "react-cookie";
 import LoggedInHome from "./components/LoggedInHome";
 import UploadSong from "./components/UploadSong";
@@ -16,7 +16,7 @@ import LikedSongView from "./components/LikedSongView";
 import Artists from "./components/Artists";
 
 
-const liked_song_playlist_id="6658a569f83c0086e6f2c930";
+// const liked_song_playlist_id="6658a569f83c0086e6f2c930";
 
 function App() {
   // gaane ko global state me store karna hai
@@ -47,10 +47,10 @@ function App() {
         ) : (
           //logged out routes
           <Routes>
-            <Route path="/home" element={<HomePage />} />
+            <Route path="/home" element={<Main />} />
             <Route path="/login" element={<LoginPage />} />
             <Route path="/signup" element={<SignupPage />} />
-            <Route path="*" element={<Navigate to="/login"/>} />
+            <Route path="*" element={<Navigate to="/home"/>} />
           </Routes>
         )}
       </BrowserRouter>
